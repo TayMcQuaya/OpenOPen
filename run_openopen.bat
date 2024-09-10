218448e3178e6897f17a6ee327efdf161a76ebe3
TayMcQuaya <taymcquaya@gmail.com>
2024-09-10 16:34:52 +0800

    Added new Bat Files and updated README File for Installing

--- run_openopen.bat
+++ run_openopen.bat
@@ -1,4 +1,9 @@
 @echo off
 python -m pip install -r requirements.txt
+
+REM Create a desktop shortcut
+echo Creating desktop shortcut...
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\OpenOPen.lnk');$s.TargetPath='%~dp0OpenOPen.bat';$s.WorkingDirectory='%~dp0';$s.Save()"
+
 python main.py
 pause
