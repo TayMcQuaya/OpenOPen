03b9d7cf8260ee59c5894958a5bbb7b4f1f2d3a9
TayMcQuaya <taymcquaya@gmail.com>
2024-09-10 14:08:35 +0800

    Remove unwanted files and keep only dist/OpenOPen folder

--- constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DEFAULT_FONT = "Times New Roman"
-DEFAULT_FONT_SIZE = 12
-WINDOW_TITLE = 'OpenOPen (v.1.0.0)'
-ICON_PATH = 'icons/app_icon.png'
-README_PATH = os.path.join('resources', 'README.md')
-LIGHT_STYLE_PATH = 'styles/style.qss'
-DARK_STYLE_PATH = 'styles/dark_style.qss'
